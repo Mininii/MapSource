62c92cd034b51f5ee65b4f511d0ac26b028a0c6e
Mininii <86014653+Mininii@users.noreply.github.com>
2021-06-21 14:33:28 +0900

    Add Comment

--- MSF_UE/LeaderBoardT.lua
+++ MSF_UE/LeaderBoardT.lua
@@ -1,6 +1,6 @@
 function LeaderBoardTFunc()
 	local LeaderBoardT = CreateCCode()
-	Trigger { -- ų ����Ʈ ��������, ����ó ���� ������Ű��, ���� ȸ��, ���۸� ����� ���ö�
+	Trigger { -- 킬 포인트 리더보드, 집근처 유닛 오더시키기, 쉴드 회복, 저글링 히드라 어택땅
 		players = {FP},
 		conditions = {
 			Label(0);
@@ -15,7 +15,7 @@ function LeaderBoardTFunc()
 			PreserveTrigger();
 		},
 	}
-	Trigger { -- ���� ���ھ� ��������
+	Trigger { -- 데스 스코어 리더보드
 		players = {FP},
 		conditions = {
 			Label(0);
@@ -29,7 +29,7 @@ function LeaderBoardTFunc()
 			PreserveTrigger();
 	},
 	}
-	Trigger { -- ų ���ھ� ��������
+	Trigger { -- 킬 스코어 리더보드
 		players = {FP},
 		conditions = {
 			Label(0);
@@ -44,8 +44,8 @@ function LeaderBoardTFunc()
 	},
 	}
 
-CIf(FP,CDeaths(FP,Exactly,200,LeaderBoardT))
-CMov(FP,0x6509B0,19025+19)
+CIf(FP,CDeaths(FP,Exactly,200,LeaderBoardT))-- 리더보드 타이머가 주기적으로 정확히 200일 경우 내린 명령이 없어 멈춰있는 유닛에 명령을 내리는 코드
+CMov(FP,0x6509B0,19025+19) --CUnit 시작지점 +19 (0x4C)
 CWhile(FP,Memory(0x6509B0,AtMost,19025+19 + (84*1699)))
 
 CIf(FP,{DeathsX(CurrentPlayer,AtLeast,1*256,0,0xFF00),DeathsX(CurrentPlayer,Exactly,7,0,0xFF)})
@@ -56,12 +56,12 @@ CIf(FP,{DeathsX(CurrentPlayer,AtLeast,1*256,0,0xFF00),DeathsX(CurrentPlayer,Exac
 			Deaths(CurrentPlayer,AtMost,0,0);
 		},
 		actions = {
-			SetDeaths(CurrentPlayer,Add,256*1,0);
+			SetDeaths(CurrentPlayer,Add,256*1,0); -- 혹시몰라서 넣어둔 좀비유닛 검사트리거. 살아있는데 체력이 0인거 1로 올림
 			PreserveTrigger();
 		}
 	}
 	DoActions(FP,MoveCp(Add,17*4))
-	CIf(FP,{TTOR({
+	CIf(FP,{TTOR({ -- 컴퓨터 유닛은 명령이 없어 대기하고 있을 경우 2, 3, 156, 160 네가지의 명령 타입으로 설정됨. 이를 전부 잡아줘야됨
 		DeathsX(CurrentPlayer,Exactly,2*256,0,0xFF00),
 		DeathsX(CurrentPlayer,Exactly,3*256,0,0xFF00),
 		DeathsX(CurrentPlayer,Exactly,156*256,0,0xFF00),
@@ -82,31 +82,31 @@ CIf(FP,{DeathsX(CurrentPlayer,AtLeast,1*256,0,0xFF00),DeathsX(CurrentPlayer,Exac
 	NJumpXEnd(FP,L_Gun_Order2)
 	f_Mod(FP,Gun_TempRand,_Rand(),_Mov(7))
 	for i = 0, 6 do
-		NIf(FP,{CVar(FP,Gun_TempRand[2],Exactly,i),PlayerCheck(i,0)})
-			CJumpX(FP,L_Gun_Order)
+		NIf(FP,{CVar(FP,Gun_TempRand[2],Exactly,i),PlayerCheck(i,0)}) -- 타겟 설정 시 플레이어가 없을 경우 다시 연산함
+			CJumpX(FP,L_Gun_Order) 
 		NIfEnd()
 	end
 	CIf(FP,CDeaths(FP,AtLeast,1,PCheck))
 	for i = 0, 6 do
-		CIf(FP,{CVar(FP,BarrackPtr[i+1][2],AtLeast,1),CVar(FP,Gun_TempRand[2],Exactly,i)})
+		CIf(FP,{CVar(FP,BarrackPtr[i+1][2],AtLeast,1),CVar(FP,Gun_TempRand[2],Exactly,i)}) -- 설정된 타겟의 배럭 좌표를 가져옴
 			CMov(FP,TempBarPos,BarPos[i+1])
 		CIfEnd()
 	end
 	CIfEnd()
-		CIf(FP,{TMemoryX(_Add(BackupCp,15),AtLeast,150*16777216,0xFF000000)})
+		CIf(FP,{TMemoryX(_Add(BackupCp,15),AtLeast,150*16777216,0xFF000000)}) -- 막혀서 유닛 안나올 경우에 명령이 들어가지 않도록 설정함. 이거안하면 캔낫 제한 줄어들면서 맵이 망가짐
 		CDoActions(FP,{
-			TSetDeathsX(_Sub(BackupCp,6),SetTo,14*256,0,0xFF00),
+			TSetDeathsX(_Sub(BackupCp,6),SetTo,14*256,0,0xFF00), -- 명령 발싸
 			TSetDeaths(_Sub(BackupCp,3),SetTo,TempBarPos,0),
 		})
 		CIfEnd()
-		local HeroPointCheck = def_sIndex()
+		local HeroPointCheck = def_sIndex() 
 		CJump(FP,HeroPointCheck)
-		NJumpXEnd(FP,Check_Hero)
+		NJumpXEnd(FP,Check_Hero) -- 영웅유닛들 도착 지점. 영작유닛인지 아닌지 체크한 후 영작유닛이 아니면 명령을 내림
 		f_SaveCp()
 		local TempCPCheck = CreateVar()
-		CMov(FP,TempCPCheck,_Sub(BackupCp,(25+19025)))
-		f_Div(FP,TempCPCheck,_Mov(84))
-		NJumpX(FP,L_Gun_Order2,{TMemory(_Add(_Mul(TempCPCheck,_Mov(0x970/4)),_Add(CC_Header,((0x20*8)/4))),AtMost,0)})
+		CMov(FP,TempCPCheck,_Sub(BackupCp,(25+19025))) 
+		f_Div(FP,TempCPCheck,_Mov(84)) -- 해당유닛의 인덱스가 몇번인지 체크함
+		NJumpX(FP,L_Gun_Order2,{TMemory(_Add(_Mul(TempCPCheck,_Mov(0x970/4)),_Add(CC_Header,((0x20*8)/4))),AtMost,0)}) -- 영작유닛이 아닐 경우 위쪽의 명령주는 트리거로 점프함.
 		CJumpEnd(FP,HeroPointCheck)
 		f_LoadCp()
 		DoActions(FP,MoveCp(Subtract,6*4))
