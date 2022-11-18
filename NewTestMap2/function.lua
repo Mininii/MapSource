d9a74731a6846e24da6cf96eb0cff1055822bd8f
DESKTOP-VPOBIQL\USER <whatdonald1@gmail.com>
2022-11-18 16:23:42 +0900

    Encoding Fix

--- NewTestMap2/function.lua
+++ NewTestMap2/function.lua
@@ -19,6 +19,7 @@ function Install_BackupCP(Player)
 		CallTrigger(Player,LoadCp_CallIndex,nil)
 	end
 end
+
 function TestSet(val)
 	if val == 1 then 
 		Limit = 1
@@ -54,7 +55,7 @@ end
 
 function SetUnitsDatX(UnitID,Property)
 	if type(UnitID) == "string" then
-		UnitID = ParseUnit(UnitID) -- ï¿½ï¿½Æ®ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½Ì¸ï¿½ ï¿½Ô·Â°ï¿½ï¿½ï¿½
+		UnitID = ParseUnit(UnitID) -- ½ºÆ®¸µÀ¸·Î À¯´ÖÀÌ¸§ ÀÔ·Â°¡´É
 	end
 	if UnitID>=228 then PushErrorMsg("UnitID Index Overflow") end
 	if type(Property)~= "table" then
@@ -62,7 +63,7 @@ function SetUnitsDatX(UnitID,Property)
 	else
 		for j,k in pairs(Property) do
 			if j=="MinCost" then
-				PatchInsert(SetMemoryW(0x663888 + (UnitID *2),SetTo,k)) -- ï¿½Ì³×¶ï¿½
+				PatchInsert(SetMemoryW(0x663888 + (UnitID *2),SetTo,k)) -- ¹Ì³×¶ö
 			elseif j=="Playerable" then
 				local SType
 				if type(k)=="boolean" then
@@ -83,11 +84,11 @@ function SetUnitsDatX(UnitID,Property)
 				PatchInsert(SetMemoryB(0x57F27C + (6 * 228) + UnitID,SetTo,SType))
 				PatchInsert(SetMemoryB(0x57F27C + (7 * 228) + UnitID,SetTo,SType))
 			elseif j=="GasCost"  then
-				PatchInsert(SetMemoryW(0x65FD00 + (UnitID *2),SetTo,k)) -- ï¿½ï¿½ï¿½ï¿½
+				PatchInsert(SetMemoryW(0x65FD00 + (UnitID *2),SetTo,k)) -- °¡½º
 			elseif j=="BuildTime"  then
-				PatchInsert(SetMemoryW(0x660428 + (UnitID *2),SetTo,k)) -- ï¿½ï¿½ï¿½ï¿½Óµï¿?
+				PatchInsert(SetMemoryW(0x660428 + (UnitID *2),SetTo,k)) -- »ý»ê¼Óµµ
 			elseif j=="SuppCost"  then
-				PatchInsert(SetMemoryB(0x663CE8 + UnitID,SetTo,k*2)) -- ï¿½ï¿½ï¿½ï¿½
+				PatchInsert(SetMemoryB(0x663CE8 + UnitID,SetTo,k*2)) -- ¼­ÇÃ
 			elseif j=="HP"  then
 				PatchInsert(SetMemory(0x662350 + (UnitID*4),SetTo,k*256))
 			elseif j=="Shield"  then
@@ -103,15 +104,15 @@ function SetUnitsDatX(UnitID,Property)
 					PatchInsert(SetMemoryW(0x660E00 + (UnitID *2), SetTo, k))
 				end
 			elseif j=="Armor" then
-				PatchInsert(SetMemoryB(0x65FEC8 + (UnitID),SetTo,k)) -- ï¿½ï¿½ï¿½ï¿½
+				PatchInsert(SetMemoryB(0x65FEC8 + (UnitID),SetTo,k)) -- ¹æ¾î·Â
 			elseif j=="GroupFlag" then
-				PatchInsert(SetMemoryB(0x6637A0 + (UnitID),SetTo,k)) -- ï¿½×·ï¿½
+				PatchInsert(SetMemoryB(0x6637A0 + (UnitID),SetTo,k)) -- ±×·ì
 			elseif j=="Height" then
-				PatchInsert(SetMemoryB(0x663150 + (UnitID),SetTo,k)) -- ï¿½Ç¼ï¿½Å©ï¿½ï¿½
+				PatchInsert(SetMemoryB(0x663150 + (UnitID),SetTo,k)) -- °Ç¼³Å©±â
 			elseif j=="BdDimX" then
-				PatchInsert(SetMemoryX(0x662860 + (UnitID*4),SetTo,k,0xFFFF)) -- ï¿½Ç¼ï¿½Å©ï¿½ï¿½
+				PatchInsert(SetMemoryX(0x662860 + (UnitID*4),SetTo,k,0xFFFF)) -- °Ç¼³Å©±â
 			elseif j=="BdDimY" then
-				PatchInsert(SetMemoryX(0x662860 + (UnitID*4),SetTo,k*65536,0xFFFF0000)) -- ï¿½Ç¼ï¿½Å©ï¿½ï¿½
+				PatchInsert(SetMemoryX(0x662860 + (UnitID*4),SetTo,k*65536,0xFFFF0000)) -- °Ç¼³Å©±â
 			elseif j=="SizeL" then
 				PatchInsert(SetMemoryX(0x6617C8 + (UnitID*8),SetTo,(k),0xFFFF))
 			elseif j=="SizeU" then
@@ -121,7 +122,7 @@ function SetUnitsDatX(UnitID,Property)
 			elseif j=="SizeD" then
 				PatchInsert(SetMemoryX(0x6617CC + (UnitID*8),SetTo,(k*65536),0xFFFF0000))
 			elseif j=="SuppProv" then
-				PatchInsert(SetMemoryB(0x6646C8+UnitID,SetTo,Property.SuppProv)) -- ï¿½ï¿½ï¿½Ã°ï¿½ï¿½Þ·ï¿½
+				PatchInsert(SetMemoryB(0x6646C8+UnitID,SetTo,Property.SuppProv)) -- ¼­ÇÃ°ø±Þ·®
 			elseif j=="AdvFlag" then
 				if type(k)~= "table" or #k~=2 then
 					PushErrorMsg("AdvFlag Inputdata Error")
@@ -199,38 +200,38 @@ function SetWeaponsDatX(WepID,Property)
 	else
 		for j,k in pairs(Property) do
 			if j=="DmgBase" then
-				PatchInsert(SetMemoryW(0x656EB0+(WepID *2),SetTo,k)) -- ï¿½ï¿½ï¿½Ý·ï¿½
+				PatchInsert(SetMemoryW(0x656EB0+(WepID *2),SetTo,k)) -- °ø°Ý·Â
 			elseif j=="DmgFactor" then
-				PatchInsert(SetMemoryW(0x657678+(WepID *2),SetTo,k)) -- ï¿½ß°ï¿½ï¿½ï¿½ï¿½Ý·ï¿½
+				PatchInsert(SetMemoryW(0x657678+(WepID *2),SetTo,k)) -- Ãß°¡°ø°Ý·Â
 			elseif j=="Cooldown" then
-				PatchInsert(SetMemoryB(0x656FB8+(WepID *1),SetTo,k)) -- ï¿½ï¿½ï¿½ï¿½
+				PatchInsert(SetMemoryB(0x656FB8+(WepID *1),SetTo,k)) -- °ø¼Ó
 			elseif j=="Splash" then
 				if type(k)=="boolean" then
 					if k == true then
-						PatchInsert(SetMemoryB(0x6566F8+WepID,SetTo,3)) -- ï¿½ï¿½ï¿½ï¿½Å¸ï¿½ï¿½(ï¿½Ï¹ï¿½ï¿½ï¿½)
+						PatchInsert(SetMemoryB(0x6566F8+WepID,SetTo,3)) -- ½ºÇÃÅ¸ÀÔ(ÀÏ¹æÇü)
 					else
-						PatchInsert(SetMemoryB(0x6566F8+WepID,SetTo,1)) -- ï¿½ï¿½ï¿½ï¿½Å¸ï¿½ï¿½(ï¿½ï¿½ï¿½Ã¾ï¿½ï¿½ï¿½)
+						PatchInsert(SetMemoryB(0x6566F8+WepID,SetTo,1)) -- ½ºÇÃÅ¸ÀÔ(½ºÇÃ¾øÀ½)
 					end
 				elseif type(k)~="table" or #k~=3 then
 					PushErrorMsg("Splash Inputdata Error")
 				else
-					PatchInsert(SetMemoryB(0x6566F8+WepID,SetTo,3)) -- ï¿½ï¿½ï¿½ï¿½Å¸ï¿½ï¿½(ï¿½Ï¹ï¿½ï¿½ï¿½)
-					PatchInsert(SetMemoryW(0x656888+(WepID*2),SetTo,k[1])) --ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½
-					PatchInsert(SetMemoryW(0x6570C8+(WepID*2),SetTo,k[2])) --ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½
-					PatchInsert(SetMemoryW(0x657780+(WepID*2),SetTo,k[3])) --ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½
+					PatchInsert(SetMemoryB(0x6566F8+WepID,SetTo,3)) -- ½ºÇÃÅ¸ÀÔ(ÀÏ¹æÇü)
+					PatchInsert(SetMemoryW(0x656888+(WepID*2),SetTo,k[1])) --½ºÇÃ ¾È
+					PatchInsert(SetMemoryW(0x6570C8+(WepID*2),SetTo,k[2])) --½ºÇÃ Áß
+					PatchInsert(SetMemoryW(0x657780+(WepID*2),SetTo,k[3])) --½ºÇÃ ¹Û
 				end
 			elseif j=="RangeMin" then
-				PatchInsert(SetMemory(0x656A18+(WepID *4),SetTo,k)) -- ï¿½ï¿½Å¸ï¿? ï¿½Ö¼ï¿½
+				PatchInsert(SetMemory(0x656A18+(WepID *4),SetTo,k)) -- »ç°Å¸® ÃÖ¼Ò
 			elseif j=="RangeMax" then
-				PatchInsert(SetMemory(0x657470+(WepID *4),SetTo,k)) -- ï¿½ï¿½Å¸ï¿? ï¿½Ö´ï¿½
+				PatchInsert(SetMemory(0x657470+(WepID *4),SetTo,k)) -- »ç°Å¸® ÃÖ´ë
 			elseif j=="TargetFlag" then
 				PatchInsert(SetMemoryW(0x657998 + (WepID*2), SetTo, k))
 			elseif j=="UpgradeType" then
 				PatchInsert(SetMemoryB(0x6571D0 + WepID, SetTo, k))
 			elseif j=="ObjectNum" then
-				PatchInsert(SetMemoryB(0x6564E0+WepID,SetTo,k)) -- ï¿½ï¿½ï¿½ï¿½Ã¼ï¿½ï¿½
+				PatchInsert(SetMemoryB(0x6564E0+WepID,SetTo,k)) -- Åõ»çÃ¼¼ö
 			elseif j=="IconType" then
-				PatchInsert(SetMemoryW(0x656780+(WepID *2),SetTo,k)) -- ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
+				PatchInsert(SetMemoryW(0x656780+(WepID *2),SetTo,k)) -- ¾ÆÀÌÄÜ
 			elseif j== "Behavior" then
 				PatchInsert(SetMemoryB(0x656670+WepID,SetTo,k))
 			elseif j== "LaunchX" then
@@ -246,7 +247,7 @@ function SetWeaponsDatX(WepID,Property)
 			elseif j== "FlingyID" then
 				PatchInsert(SetMemory(0x656CA8+(WepID *4),SetTo,k))
 			elseif j== "WepName" then
-				PatchInsert(SetMemoryW(0x6572E0+(WepID *2),SetTo,k)) -- ï¿½Ì¸ï¿½
+				PatchInsert(SetMemoryW(0x6572E0+(WepID *2),SetTo,k)) -- ÀÌ¸§
 			elseif j== "DmgType" then
 				PatchInsert(SetMemoryB(0x657258+(WepID),SetTo,k)) -- 
 				
@@ -341,7 +342,7 @@ function PopLevelUnit()
 		table.insert(CtrigInitArr[FP+1],SetMemX(Arr(LevelDataArr,j-1),SetTo,k[2]))
 	end
 	
-	SetUnitsDatX(LevelUnitArr[#LevelUnitArr][2], {DefUpType=60}) -- ï¿½Ö°ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½È­È®ï¿½ï¿½ ï¿½ï¿½ï¿½ß±ï¿½
+	SetUnitsDatX(LevelUnitArr[#LevelUnitArr][2], {DefUpType=60}) -- ÃÖ°­À¯´Ö °­È­È®·ü °¨Ãß±â
 
 	GetUnitVArr = CreateVArrArr(7, #LevelUnitArr, FP)
 end
@@ -350,11 +351,11 @@ function CIfKeyFunc(CP,Key,ContentStr,DisContentStr,Conditions,CondActions,DisCo
 	CIf(FP,{MSQC_KeyInput(CP, Key)})
 	CallTrigger(FP,Call_Print13[CP+1])
 	CTrigger(FP,{CDeaths(FP,AtMost,0,ShopKey[CP+1]),LocalPlayerID(CP),Conditions},{print_utf8(12,0,ContentStr)},{preserved})
-	CTrigger(FP,{CDeaths(FP,AtMost,0,ShopKey[CP+1]),Conditions},{SetCDeaths(FP,SetTo,1,ShopKey[CP+1]),CondActions},{preserved})	-- ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿?
+	CTrigger(FP,{CDeaths(FP,AtMost,0,ShopKey[CP+1]),Conditions},{SetCDeaths(FP,SetTo,1,ShopKey[CP+1]),CondActions},{preserved})	-- Á¶°ÇÀÌ ¸¸Á·ÇÒ °æ¿ì
 	CTrigger(FP,{CDeaths(FP,AtMost,0,ShopKey[CP+1]),LocalPlayerID(CP)},{print_utf8(12,0,DisContentStr)},{preserved})
-	CTrigger(FP,{CDeaths(FP,AtMost,0,ShopKey[CP+1])},{SetCDeaths(FP,SetTo,1,ShopKey[CP+1]),DisCondActions},{preserved})	-- ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿?
+	CTrigger(FP,{CDeaths(FP,AtMost,0,ShopKey[CP+1])},{SetCDeaths(FP,SetTo,1,ShopKey[CP+1]),DisCondActions},{preserved})	-- Á¶°ÇÀÌ ¸¸Á·ÇÏÁö ¾ÊÀ» °æ¿ì
 end
-function KeyFunc(CP,Key,ContentArgs) --{{1ï¿½ï¿½ï¿½ï¿½ï¿½Ç¹è¿­,ï¿½×¼Ç¹è¿­,ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ø½ï¿½Æ?},...}
+function KeyFunc(CP,Key,ContentArgs) --{{1Â÷Á¶°Ç¹è¿­,¾×¼Ç¹è¿­,Ãâ·ÂÇÒÅØ½ºÆ®},...}
 	CIf(FP,{MSQC_KeyInput(CP, Key)})
 	for o,p in pairs(ContentArgs) do
 		if o == 1 then
@@ -381,9 +382,9 @@ function CIfBtnFunc(CP,ID,ContentStr,DisContentStr,Conditions,CondActions,DisCon
 	CurShopCond = Conditions
 	CallTrigger(FP,Call_Print13[CP+1])
 	CTrigger(FP,{CDeaths(FP,AtMost,0,ShopSw[CP+1]),LocalPlayerID(CP),Conditions},{print_utf8(12,0,ContentStr)},{preserved})
-	CTrigger(FP,{CDeaths(FP,AtMost,0,ShopSw[CP+1]),Conditions},{SetCDeaths(FP,SetTo,1,ShopSw[CP+1]),CondActions},{preserved})	-- ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿?
+	CTrigger(FP,{CDeaths(FP,AtMost,0,ShopSw[CP+1]),Conditions},{SetCDeaths(FP,SetTo,1,ShopSw[CP+1]),CondActions},{preserved})	-- Á¶°ÇÀÌ ¸¸Á·ÇÒ °æ¿ì
 	CTrigger(FP,{CDeaths(FP,AtMost,0,ShopSw[CP+1]),LocalPlayerID(CP)},{print_utf8(12,0,DisContentStr)},{preserved})
-	CTrigger(FP,{CDeaths(FP,AtMost,0,ShopSw[CP+1])},{SetCDeaths(FP,SetTo,1,ShopSw[CP+1]),DisCondActions},{preserved})	-- ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿?
+	CTrigger(FP,{CDeaths(FP,AtMost,0,ShopSw[CP+1])},{SetCDeaths(FP,SetTo,1,ShopSw[CP+1]),DisCondActions},{preserved})	-- Á¶°ÇÀÌ ¸¸Á·ÇÏÁö ¾ÊÀ» °æ¿ì
 end
 
 function BtnSetInit(CP,MenuPtr)
@@ -501,11 +502,11 @@ function Debug_DPSBuilding(UnitPtrDest,BuildingID,BuildingLoc)
 	CIfEnd()
 
 end
-function MSQC_KeySet(KeyName,DeathUnit) -- Å°ï¿½Î½Ä¿ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿?
+function MSQC_KeySet(KeyName,DeathUnit) -- Å°ÀÎ½Ä¿ë µ¥½º°ª µî·Ï
 	MSQC_KeyArr[KeyName] = DeathUnit
 end
 
-function MSQC_KeyInput(Player,KeyName) -- Å°ï¿½Î½Ä¿ï¿½ ï¿½ï¿½ï¿½ï¿½
+function MSQC_KeyInput(Player,KeyName) -- Å°ÀÎ½Ä¿ë Á¶°Ç
 	return Deaths(Player, AtLeast, 1, MSQC_KeyArr[KeyName])
 end
 function MSQC_ExportEdsTxt()
